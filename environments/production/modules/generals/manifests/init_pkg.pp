4cd0c2fa86ff5fe9545eecbe6175bbcee1cc7f4e
root <root@ns347092.ip-91-121-160.eu>
2017-12-27 09:45:35 +0000

    6  commit

--- environments/production/modules/generals/manifests/init_pkg.pp
+++ environments/production/modules/generals/manifests/init_pkg.pp
@@ -4,6 +4,6 @@ exec { 'apt-get update':
     command => '/usr/bin/apt-get update'
 }
 
-$logiciels = [ 'screen', 'strace', 'sudo', 'vim', 'curl', 'htop', 'git', 'telnet', 'make', 'python-setuptools', 'gcc', 'python-dev', 'libffi-dev', 'libssl-dev']
+$logiciels = [ 'screen', 'strace', 'sudo', 'vim', 'curl', 'htop', 'git', 'telnet', 'make', 'python-setuptools', 'gcc', 'python-dev', 'libffi-dev', 'libssl-dev', 'ncdu']
 package { $logiciels: ensure => 'installed', require => Exec['apt-get update'] }
 }
